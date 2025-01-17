4a563b77ea9ab6fdb800f10f4af7b84eca6f8c16
kanishk44 <kanishk.shedsale@gmail.com>
2025-01-17 20:14:21 +0530

    Added model

--- controllers/products.js
+++ controllers/products.js
@@ -2,15 +2,20 @@ const path = require("path");
 
 const rootDir = require("../util/path");
 
+const Product = require("../models/product");
+
 exports.getAddProducts = (req, res, next) => {
   res.sendFile(path.join(rootDir, "views", "add-product.html"));
 };
 
 exports.postAddProducts = (req, res, next) => {
-  console.log(req.body);
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
+  const products = Product.fetchAll();
+  console.log(products);
   res.sendFile(path.join(rootDir, "views", "shop.html"));
 };
